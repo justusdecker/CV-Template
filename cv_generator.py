4153ef14df6c2c6b294a1ea82e695a3ed4ddf841
Justus <justus.d2025@gmail.com>
2025-11-29 12:24:19 +0100

    fixed loader issues & added CSS embed

--- cv_generator.py
+++ cv_generator.py
@@ -2,9 +2,14 @@ import jinja2
 from src.cvc_settings import CVC
 
 def generate(filepath: str, settings: CVC = CVC):
-    ENV = jinja2.Environment()
-    template = ENV.get_template('template.html')
+    ENV = jinja2.Environment(loader=jinja2.FileSystemLoader('./src/template/'))
+    template = ENV.get_template('index.html')
+    
+    with open(f'./src/static/styles.css', 'rb') as f:
+        CSS = f.read().decode()
     
     with open(filepath, 'wb') as f:
-        f.write(template.render( data = settings ).encode())
-    
+        f.write(template.render( data = settings, enumerate = enumerate, css = CSS ).encode())
+
+
+generate('test.html')
